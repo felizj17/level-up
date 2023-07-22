03eb86455d82ad32e9574e8b9a22ef51704e7108
SuperNinjaEv <evan0210@gmail.com>
2023-07-22 16:19:42 -0400

    Completed API Calls

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
+
+// API Assignments for Axios
 import axios from 'axios';
 const API_KEY = process.env.APP_TOKEN;
+// axios.defaults.headers.common['Authorization'] = API_KEY;
 const API_FIN = process.env.REACT_APP_API_FINANCIAL_EMPOWERMENT;
 const API_COU = process.env.REACT_APP_API_COURSES_TRAINING;
 
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
+  const [finPosts, setFinPosts] = useState([]);
+  const [coursePosts, setCoursePosts] = useState([]);
+  const [taxPosts, setTaxPosts] = useState([]);
+
 
   useEffect(() => {
-    axios.get('https://data.cityofnewyork.us/resource/fgq8-am2v.json')
+    // Financial Empowerment Get
+    axios.get(API_FIN)
       .then(response => {
-        setPosts(response.data);
+        setFinPosts(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        // console.error(error);
+      });
+      // Training Courses Get
+      axios.get(API_COU)
+      .then(response => {
+        setCoursePosts(response.data);
+        // console.log(response.data)
       })
       .catch(error => {
         console.error(error);
       });
+      // Free Tax Center Get
+      axios.get('https://data.cityofnewyork.us/resource/5kqf-fg3n.json')
+      .then(response => {
+        setTaxPosts(response.data);
+        // console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
   }, []);
 
 
