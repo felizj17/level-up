6c6662542024361c369e7d3926b9ef5251554769
SuperNinjaEv <evan0210@gmail.com>
2023-07-23 13:23:43 -0400

    Removed axios calls from App.js and placed in Centers.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import Home from './components/Home';
 import './App.css';
 
-// API Assignments for Axios
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-const API_KEY = process.env.APP_TOKEN;
-axios.defaults.headers.common['Authorization'] = API_KEY;
-const API_FIN = process.env.REACT_APP_API_FINANCIAL_EMPOWERMENT;
-
 function App() {
-
-  const [finPosts, setFinPosts] = useState([]);
-
-  useEffect(() => {
-      // Financial Empowerment Get
-      axios.get(API_FIN)
-          .then(response => {
-              setFinPosts(response.data);
-              console.log(response.data)
-          })
-          .catch(error => {
-              console.error(error);
-          });
-  }, []);
 
   return (
     <div className='App'>
