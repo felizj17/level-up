9c291712c136a72398e113de3f6c35efb1b0cab9
SharukhGV <sharukhvelupillai@pursuit.org>
2023-07-23 12:29:08 -0400

    quiz and form in componeents folder

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Home from './components/Home';
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import React, { useEffect, useState } from 'react';
-import Quiz from "./Quiz"
-// // API Assignments for Axios
-// import axios from 'axios';
-// const API_KEY = process.env.APP_TOKEN;
-// axios.defaults.headers.common['Authorization'] = API_KEY;
-// const API_FIN = process.env.REACT_APP_API_FINANCIAL_EMPOWERMENT;
-// // const API_COU = process.env.REACT_APP_API_COURSES_TRAINING;
+
+// API Assignments for Axios
+import axios from 'axios';
+const API_KEY = process.env.APP_TOKEN;
+axios.defaults.headers.common['Authorization'] = API_KEY;
+const API_FIN = process.env.REACT_APP_API_FINANCIAL_EMPOWERMENT;
+// const API_COU = process.env.REACT_APP_API_COURSES_TRAINING;
 
 
 function App() {
 
-  // const [finPosts, setFinPosts] = useState([]);
+  const [finPosts, setFinPosts] = useState([]);
   // const [coursePosts, setCoursePosts] = useState([]);
 
 
-  // useEffect(() => {
-  //   // Financial Empowerment Get
-  //   axios.get(API_FIN)
-  //     .then(response => {
-  //       setFinPosts(response.data);
-  //       console.log(response.data)
-  //     })
-  //     .catch(error => {
-  //       // console.error(error);
-  //     });
-  //     // Training Courses Get
-  //     // axios.get(API_COU)
-  //     // .then(response => {
-  //     //   setCoursePosts(response.data);
-  //     //   console.log(response.data)
-  //     // })
-  //     // .catch(error => {
-  //     //   console.error(error);
-  //     // });
+  useEffect(() => {
+    // Financial Empowerment Get
+    axios.get(API_FIN)
+      .then(response => {
+        setFinPosts(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        // console.error(error);
+      });
+      // Training Courses Get
+      // axios.get(API_COU)
+      // .then(response => {
+      //   setCoursePosts(response.data);
+      //   console.log(response.data)
+      // })
+      // .catch(error => {
+      //   console.error(error);
+      // });
      
-  // }, []);
+  }, []);
 
 
   return (
-    <div className="App">
-<Quiz/>    </div>
-  );
+    <div className='App'>
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
